aa372391dde8c2a999f90f263015a4d723cd364b
umasunshine002 <125578882+umasunshine002@users.noreply.github.com>
2025-05-08 14:11:52 -0400

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,9 +7,6 @@ const Footer = () => {
       <div className="container mx-auto px-4">
         <div className="flex flex-col md:flex-row justify-between items-center">
           <div className="mb-6 md:mb-0">
-            <a href="#" className="text-xl font-bold gradient-text">
-              Umadevi Thulluru<span className="text-foreground">.dev</span>
-            </a>
           </div>
           
           <div className="flex space-x-6 mb-6 md:mb-0">
